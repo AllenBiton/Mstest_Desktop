788030901aff0195eb112a9f5f380d010b2e4d0c
AllenBiton <bitonallen11@gmail.com>
2023-02-08 13:58:55 +0200

    sdf

---
+++ Mstest_Desktop2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mstest_Desktop2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mstest_Desktop2")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a50a5677-6e85-4b07-b457-68563eedbe01")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Mstest_Desktop2/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+
+using TypeMock.ArrangeActAssert;
+
+namespace Mstest_Desktop2
+{
+    [TestClass]
+    public class MathTests
+    {
+
+        [TestMethod]
+        public void IsEvenNum_Test_ReturnsTrue()
+        {
+
+            Mathe obj = new Mathe();
+
+            Isolate.WhenCalled(() => obj.IsEvenNum(0)).WillReturn(true);
+            // act
+            var result = obj.IsEvenNum((8));
+
+            // assert
+            Assert.AreEqual(true, result);
+        }
+
+    }
+    public class Mathe
+    {
+        int num1;
+        int num2;
+
+        public Mathe()
+        {
+        }
+        public bool IsEvenNum(int number)
+        {
+            var isEven = (number % 2 == 0) ? true : false;
+            return isEven;
+        }
+    }
+}
